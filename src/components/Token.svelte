85a6895c581502d9d77167f1c7b7fe576c70dd13
Chris-Robin Ennen <chris@ncteam.de>
2020-07-28 14:14:43 +0200

    add transition to token

--- src/components/Token.svelte
+++ src/components/Token.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import { push } from "svelte-spa-router"
+  import { fade } from "svelte/transition"
 
   let token: string = localStorage.getItem("token")
 
   function saveToken() {
     localStorage.setItem("token", token)
-    push('/')
+    push("/")
   }
 </script>
 
-<textarea class="border my-4" placeholder="Paste your token here" rows={16} bind:value={token} />
-<button class="btn btn-blue" type="button" on:click={saveToken}>Save</button>
+<div in:fade class="flex flex-col">
+  <textarea class="border my-4" placeholder="Paste your token here" rows={16} bind:value={token} />
+  <button class="btn btn-blue" type="button" on:click={saveToken}>Save</button>
+</div>
